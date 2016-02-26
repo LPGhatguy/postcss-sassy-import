de26b455ee4c52a7a4939daebfb583b3e4a18885
Lucien Greathouse <me@lpghatguy.com>
2016-02-26 15:33:36 -0700

    Initial commit :boom:

---
+++ README.md
@@ -0,0 +1,171 @@
+# postcss-sassy-import
+	A sassy import targeted at PostCSS + Sass pipelines
+
+```sh
+npm install postcss-sassy-import
+```
+
+postcss-sassy-import is an alternative to a number of import possibilities:
+- Stock SCSS imports
+- node-sass import plugins (like [node-sass-glob-once](https://github.com/LPGhatguy/node-sass-glob-once))
+- [postcss-import](https://github.com/postcss/postcss-import)
+
+postcss-sassy-import is not limited to SCSS, however! It can import vanilla CSS, JSON, or most anything else with a little bit of extension.
+
+## Why not Sass imports?
+Stock Sass imports don't handle globbing or single-file imports. Even with node-sass import plugins, mixing and matching functionality is difficult and error-prone.
+
+## Why not postcss-import?
+The authors of postcss-import are trying to [follow the CSS specification](https://github.com/postcss/postcss-import/issues/176#issuecomment-188995732) somewhat, which means that support for extra features is second-class.
+
+## Usage
+Write imports how you want to write them:
+
+```scss
+@import "config.json";
+@import "reset";
+@import "components/**/*.scss";
+
+my-scope {
+	@import "scoped-stuff";
+}
+```
+
+Get output like you expect:
+
+```scss
+/* contents of config.json */
+/* contents of reset.css */
+/* contents of all scss files in components/ */
+
+my-scope {
+	/* contents of _scoped-stuff.scss */
+}
+```
+
+Sass (or another preprocessor) can then handle the rest of the processing from there.
+
+postcss-sassy-import will import files based on globs, and only import files once by default. This lets you declare stylesheet dependencies sanely and get no duplicates!
+
+postcss-sassy-import can import plenty of useful things out of the box:
+- Sass (SCSS syntax)
+- Sass Partials (SCSS syntax)
+- CSS
+- JSON (as SCSS variables)
+
+It's also possible to force reuse of imports, which will cascade into their imports as well:
+
+`main.scss`
+```scss
+first-scope {
+	@import "b";
+}
+
+second-scope {
+	@import "b";
+}
+
+third-scope {
+	@import "b" !multiple;
+}
+```
+
+`b.scss`
+```scss
+@import "c";
+
+a {
+	color: red;
+}
+```
+
+`c.scss`
+```scss
+div {
+	color: blue;
+}
+```
+
+Results in:
+```scss
+first-scope {
+	div {
+		color: blue;
+	}
+
+	a {
+		color: red;
+	}
+}
+
+second-scope {
+}
+
+third-scope {
+	div {
+		color: blue;
+	}
+
+	a {
+		color: red;
+	}
+}
+```
+
+### gulp
+If you use gulp, use postcss-sassy-import with gulp-postcss:
+
+```js
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const sass = require("gulp-sass");
+const sassyImport = require("postcss-sassy-import");
+
+gulp.task("styles", () => {
+	return gulp.src("main.scss")
+		.pipe(postcss([ sassyImport({
+			// Put your configuration here!
+		}) ]))
+		.pipe(sass())
+		.pipe(gulp.dest("main.css"))
+});
+```
+
+## Configuration
+postcss-sassy-import takes in some configuration options and does some stuff with them.
+
+#### dedup
+- `boolean`
+- default: `true`
+
+Enable to deduplicate imports by default. Opt in and out of deduplication by using the `!once` and `!multiple` flags on imports, respectively.
+
+#### formats
+- `string[]`
+- default: `["%", "%.scss", "_%.scss", "%.css", "%.json", "%/style.scss"]`
+
+A list of formats to try to resolve a path with. `%` in the string is replaced with the file name, and the directory of the file is prepended to the result.
+
+#### loaders
+- `array`
+- default: see source (loads scss, css, and json)
+
+A loader is an object with two fields, `test` and `method`. `test` is a function taking the path of the file, returning whether this loader is applicable. `method` is a method to return a PostCSS `Root` element from a wrapped file.
+
+Wrapped files have two fields:
+- `contents`: The contents of the file
+- `path`: The path to the file
+
+#### plugins
+- `array`
+- default: `[]`
+
+Plugins are run when postcss-sassy-import is called. They are passed the options that were given and are expected to mutate them.
+
+This is a good place to add new loaders and formats.
+
+## Contributing
+Pull requests and issues welcome!
+
+## License
+postcss-sassy-import is licnsed under the MIT license. See [LICENSE.md](LICENSE.md) for more details.
---
+++ index.js
@@ -0,0 +1,254 @@
+/**
+ * PostCSS Sassy Import
+ * Implements imports like Sass, plus:
+ * - Import only one time by default
+ * - Import Sass, CSS, and JSON files
+ */
+
+"use strict";
+
+// Our plugin!
+let plugin;
+
+const path = require("path");
+const glob = require("glob");
+
+const fsUtil = require("./lib/fs-util");
+const toSCSS = require("./lib/to-scss");
+
+const postcss = require("postcss");
+const syntaxSCSS = require("postcss-scss");
+
+/**
+ * Takes a resolved file path and returns the correct loader for it, if one is found.
+ */
+function getLoader(loaders, filePath) {
+	for (const loader of loaders) {
+		if (loader.test(filePath)) {
+			return loader.method;
+		}
+	}
+}
+
+/**
+ * A promiseified version of glob
+ */
+function globPromise(dir) {
+	return new Promise((resolve, reject) => {
+		glob(dir, (err, matches) => {
+			if (err) {
+				return reject(err);
+			}
+
+			resolve(matches);
+		});
+	});
+}
+
+plugin = postcss.plugin("postcss-sassy-import", function(opts) {
+	opts = Object.assign({}, opts) || {};
+
+	// Files that have already been loaded
+	const loaded = opts.loaded || new Set();
+	opts.loaded = loaded;
+
+	const dedup = opts.dedup != null ? opts.dedup : true;
+
+	// File formats we accept
+	const formats = opts.formats || [
+		"%", // full file path
+		"%.scss", // SCSS
+		"_%.scss", // SCSS partial
+		"%.css", // CSS
+		"%.json", // JSON data (Sass variables)
+		"%/style.scss" // Folder containing SCSS
+	];
+
+	opts.formats = formats;
+
+	// Loaders based on file
+	// For these loaders, the only plugin PostCSS needs is this import plugin.
+	// All other plugins will run over the source afterwards.
+	const loaders = opts.loaders || [
+		{
+			test: (file) => /\.scss$/.test(file),
+			method: (wrapped, opts) => {
+				opts = opts || {};
+
+				return postcss([ plugin(opts) ])
+					.process(wrapped.contents, {
+						from: wrapped.path,
+						syntax: syntaxSCSS
+					});
+			}
+		},
+		{
+			test: (file) => /\.css$/.test(file),
+			method: (wrapped, opts) => {
+				opts = opts || {};
+
+				return postcss([ plugin(opts) ])
+					.process(wrapped.contents, {
+						from: wrapped.path
+					});
+			}
+		},
+		{
+			test: (file) => /\.json$/.test(file),
+			method: (wrapped, opts) => {
+				const data = JSON.parse(wrapped.contents);
+
+				const scss = toSCSS.fromRootObject(data);
+
+				return postcss([ plugin(opts) ])
+					.process(scss, {
+						from: wrapped.path,
+						syntax: syntaxSCSS
+					});
+			}
+		}
+	];
+
+	opts.loaders = loaders;
+
+	if (opts.plugins) {
+		opts.plugins.forEach(plugin => {
+			plugin(opts);
+		});
+
+		delete opts.plugins;
+	}
+
+	return (css, result) => {
+		// Returns Promise<PostCSSNode>
+		const processNormal = (unresolved, options) => {
+			const dedup = options.dedup;
+
+			let mergedOpts = Object.assign({}, opts, options);
+
+			return Promise.resolve()
+				.then(() => {
+					const possible = fsUtil.resolvePath(formats, unresolved);
+
+					return fsUtil.getFileOneOf(possible);
+				})
+				.then((file) => {
+					if (dedup && loaded.has(file.path)) {
+						return;
+					}
+
+					loaded.add(file.path);
+
+					const loader = getLoader(loaders, file.path);
+
+					return loader(file, mergedOpts)
+						.then(r => r.root);
+				});
+		};
+
+		// Returns Promise<PostCSSNode[]>
+		const processGlob = (unresolved, options) => {
+			const dedup = options.dedup;
+
+			let mergedOpts = Object.assign({}, opts, options);
+
+			return Promise.resolve()
+				.then(() => globPromise(unresolved))
+				.then(matches => {
+					// Early-out for globbed files
+					if (dedup) {
+						matches = matches.filter(file => !loaded.has(file));
+					}
+
+					return fsUtil.getFilesAllOf(matches);
+				}).then(matches => {
+					// Progressively build up a PostCSSNode[]
+					let prom = Promise.resolve([]);
+
+					for (const match of matches) {
+						const loader = getLoader(loaders, match.path);
+
+						// TODO: add config option for this
+						if (!loader) {
+							console.warn("Couldn't find loader for ", match.path, " -- skipping...");
+							continue;
+						}
+
+						prom = prom.then((result) => {
+							// This file could've been loaded as a dep earlier in the glob
+							if (dedup && loaded.has(match.path)) {
+								return result;
+							}
+
+							loaded.add(match.path);
+
+							return loader(match, mergedOpts)
+								.then(res => {
+									result.push(res);
+
+									return result;
+								});
+						});
+					}
+
+					return prom
+						.then(results => results.map(r => r.root));
+				});
+		};
+
+		// Our workhorse!
+		return new Promise((resolve, reject) => {
+			let prom = Promise.resolve();
+
+			css.walkAtRules("import", (node) => {
+				// Strip off quotes, single and double
+				const matches = node.params.trim().match(/^['"](.+?)['"](.*)/);
+
+				const modes = matches[2].trim().split(" ");
+				const fragment = matches[1];
+
+				let dedupThis = dedup;
+
+				if (modes.indexOf("!once") > -1) {
+					dedupThis = true;
+				} else if (modes.indexOf("!multiple") > -1) {
+					dedupThis = false;
+				}
+
+				const origin = node.source.input.file;
+
+				const unresolved = path.normalize(path.join(path.dirname(origin), fragment));
+
+				if (glob.hasMagic(fragment)) {
+					prom = prom.then(() => {
+						return processGlob(unresolved, {
+							dedup: dedupThis
+						});
+					}).then(newNodes => {
+						if (newNodes.length > 0) {
+							node.replaceWith(...newNodes);
+						} else {
+							node.remove();
+						}
+					});
+				} else {
+					prom = prom.then(() => {
+						return processNormal(unresolved, {
+							dedup: dedupThis
+						});
+					}).then(newNode => {
+						if (newNode) {
+							node.replaceWith(newNode);
+						} else {
+							node.remove();
+						}
+					});
+				}
+			});
+
+			resolve(prom);
+		});
+	};
+});
+
+module.exports = plugin;
